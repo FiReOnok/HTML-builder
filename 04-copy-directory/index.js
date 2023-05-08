544b073d0c9bb303d3a98cf86a880453f1dd5e43
Yakov Kungurtsev <fireonok@gmail.com>
2023-05-08 06:33:48 +0400

    feat: complete task '04-copy-directory'

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const { join } = require('node:path');
+const { copyFile, mkdir, readdir, rm } = require('node:fs/promises')
 
+async function copyDir() {
+  const dirFrom = join(__dirname, 'files');
+  const dirTo = join(__dirname, 'files-copy');
+  await rm(dirTo, { force: true, recursive: true }); // remove folder to actualize removed files
+  await mkdir(dirTo, { recursive: true });
+  readdir(dirFrom)
+    .then(files => {
+      for (const file of files) {
+        copyFile(join(dirFrom, file), join(dirTo, file));
+      }
+    });
+};
+
+copyDir().catch(console.error);
